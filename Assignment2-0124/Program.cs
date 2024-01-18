afad50d23c657300dae99d3da4c168076f8602dc
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2024-01-18 14:49:36 +0100

    Added interesting loop, final commit

--- Assignment2-0124/Program.cs
+++ Assignment2-0124/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Beware, complex loop below
+        int num = 10;
+        for (int i = 0; i <= num; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+
         Console.WriteLine("Hello, World!");
         Hello("Sebastian");
     }
